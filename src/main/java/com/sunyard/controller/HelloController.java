7fe890fb1e423582e38ee03a02dbbd4f34ae65a7
yurisakabishou <395147048@qq.com>
2019-08-04 23:43:38 +0800

    1.2  1

--- src/main/java/com/sunyard/controller/HelloController.java
+++ src/main/java/com/sunyard/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.sunyard.controller;
 
 public class HelloController {
 
+    private String name;
 
     public HelloController(){
         System.out.println("HelloController.HelloController");
