d2fc943d3fec5e2cf888fe19b067bf2fd4eee3bf
yurisakabishou <395147048@qq.com>
2019-08-06 23:05:07 +0800

    1.5

--- src/main/java/com/sunyard/controller/HelloController.java
+++ src/main/java/com/sunyard/controller/HelloController.java
@@ -12,8 +12,15 @@ public class HelloController {
 
     private int c; //1.4
 
+    private int d; //1.5 用于测试
+
     public HelloController(){
         System.out.println("HelloController.HelloController");
+    }
+
+    // 1.5
+    private void test(){
+        System.out.println("d = " + d);
     }
 
 }
